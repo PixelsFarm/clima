48241de4a4534f3a8d0e47996d3277422c42d9dc
hector <h.pascual@ondho.com>
2020-11-24 00:07:04 +0100

    Crear state para guardar datos del formulario con el evento onchange

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    const {ciudad, pais} = busqueda;
+
+    const handleChange = e => {
+        e.preventDefault();
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form>
             <div className="input-field col s12">
@@ -8,6 +23,8 @@ const Formulario = () => {
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="ciudad">Ciudad: </label>
             </div>
@@ -15,8 +32,17 @@ const Formulario = () => {
                 <select
                     name="pais"
                     id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >   
-                    <option value=""> --Selecciona un país</option>
+                    <option value="">-- Selecciona un país</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">País: </label>
             </div>
